45410fb174aadc5f92f952e8aba5bb42e413a057
Ankit Ydv <innocentankit1141@gmail.com>
2023-04-17 23:30:08 +0530

    add: imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import './App.css'
+import Home from "./pages/Home"
+import ExerciseDetail from "./pages/ExerciseDetail"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { x1: '1488px' } }}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
