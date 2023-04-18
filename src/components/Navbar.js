f7ca1f1b325286e0099a79a3aed03357ed24d381
Ankit Ydv <innocentankit1141@gmail.com>
2023-04-18 09:46:10 +0530

    add: links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+import Logo from './../assets/images/Logo.png'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack
+      direction='row'
+      justifyContent="space-around"
+      sx={{gap:{sm: '122px', xs:'20px'}, mt:{sm: '32px', xs:'20px'}, justifyContent:'none'}}
+      px='20px'
+    >
+      <Link>
+      <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px'}} />
+      </Link>
+      <Stack
+        direction='row'
+        gap="30px"
+        fontSize="18px"
+        alignItems="flex-end"
+      >
+        <Link to='/' style={{textDecoration: 'none', color:'#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
+        <a href="#exercises" style={{textDecoration:'none', color: '#3A1212'}}>Exercises</a>
+      </Stack>
+    </Stack>
   )
 }
 
