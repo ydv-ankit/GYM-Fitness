809a36e6b898a0a519e77bf816dab93bc8d9fe0a
Ankit Ydv <innocentankit1141@gmail.com>
2023-04-18 21:46:00 +0530

    add: elements

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Box, Stack, Typography, Button} from '@mui/material'
+import HeroBannerImage from './../assets/images/banner.png'
 
 const HeroBanner = () => {
   return (
@@ -13,17 +14,28 @@ const HeroBanner = () => {
         <Typography
             fontWeight={700}
             sx={{ fontSize: { lg: '44px', xs:'40px' }}}
+            mb="23px" mt="33px"
         >
             Sweat, Smile <br/> and Repeat
         </Typography>
         <Typography
             fontSize="22px"
-            lineHeight="35px"
-        >
+            lineHeight="35px" mb={3}>
             Checkout the most efective exercises
         </Typography>
-        <Button>Explore Exercises</Button>
+        <Button href='#exercises' variant='contained' color='error' sx={{backgroundColor: '#ff2625', padding:"20px"}}>Explore Exercises</Button>
+        <Typography 
+          fontWeight={600} 
+          color='#ff2625' 
+          sx={{
+              opacity: 0.1,
+              display: {lg: 'block', xs: 'none'}
+            }} 
+          fontSize="200px"
+          >Exercises</Typography>
+        <img src={HeroBannerImage} alt="banner" className='hero-banner-img' />
     </Box>
+    
   )
 }
 
