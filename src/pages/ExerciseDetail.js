5a5c9006f408d7fb86e7da9e4eb871e57a9723ab
Ankit Ydv <innocentankit1141@gmail.com>
2023-04-21 17:37:52 +0530

    add: exercises components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { exerciseOptions, fetchData} from '../utils/fetchData'
+import Detail from '../components/Detail'
+import SimilarExercises from '../components/SimilarExercises'
+import ExerciseVideos from '../components/ExerciseVideos'
 
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail  />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
