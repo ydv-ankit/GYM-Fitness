0f7807ec0944c6d9cba7095bf6a89659daf1e559
Ankit Ydv <innocentankit1141@gmail.com>
2023-04-22 18:19:31 +0530

    add: youtube API options

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,11 +2,19 @@ export const exerciseOptions = {
     method: 'GET',
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     }
 
 }
+
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    }
+};
 
 export const fetchData = async (url, options) => {
     const reponse = await fetch(url, options);
