9203bdd311bf90eb021f26a861e3e9e922ef58f9
Ankit Ydv <innocentankit1141@gmail.com>
2023-04-22 18:20:39 +0530

    add: fetch videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Typography, Box, Stack } from '@mui/material'
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({exerciseVideos, name}) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{mt: { lg: '200px', xs:'20px'}}} p='20px' >
+      <Typography variant='h3' mb='33px'>
+        Watch <span style={{color:'#ff2625', textTransform:'capitalize'}}> {name} </span> videos
+      </Typography>
+      <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
+        sx={{
+          flexDirection: { lg: 'row'},
+          gap:{ lg: '110px', xs:'0'}
+        }} >
+
+      </Stack>
+    </Box>
   )
 }
 
